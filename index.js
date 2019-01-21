54d64330958a18078436de4bf920bb8278448f59
Will Harris <Harriswill22@yahoo.com>
2019-01-21 18:59:38 -0500

    Value vs Reference

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ function Circle(radius) {
 const another = new Circle(1);
 
 
+// Value vs Reference 
+
+
+// Primitives are copied by their value 
+// Object,Function,Array
+
+// Objects are copied by their reference
+// Number,String, Boolean, Symbol,undefined,null
+
+// let x = {value: 10}
+// let y = x;
+
+// x.value = 20;
+
+let obj = {value:10};
+function increase (obj) { 
+    obj.value++;
+}
+increase(obj);
+console.log(obj);
